9f87b43840a35c0a82aca48f41c73f94d680a6e1
unknown <gastondastan25@gmail.com>
2023-12-31 20:36:54 +0300

    help_desk

---
+++ lib/home.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:helpdesk/settingscreen.dart';
+import 'loginscreen.dart';
+import 'dashboardscreen.dart';
+import 'ticket_submission_screen.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Help Desk'),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(19, 6, 107, 147),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: 'Settings Icon',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsScreen()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const Text(
+              'Welcome!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the Login Screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginScreen()),
+                );
+              },
+              child: const Text('Login Screen'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the Dashboard Screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const DashboardScreen()),
+                );
+              },
+              child: const Text('Dashboard'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the Ticket Submission Screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const TicketSubmissionScreen()),
+                );
+              },
+              child: const Text('Submit Ticket'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
